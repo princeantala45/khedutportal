1adf6915230929402a68fcdea54d53488cd1cc78
Prince <princeantala45@gmail.com>
2026-01-09 14:45:43 +0530

    Fix static files configuration

--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -132,13 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-# added manually 
-STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-]
 
+STATIC_URL = '/static/'
+
+# Where collectstatic puts files (production)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Where your source static files live (development)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
