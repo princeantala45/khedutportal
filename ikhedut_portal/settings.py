14898b93afef4c3a13e6af83820876e8ccdb7cc5
Prince <princeantala45@gmail.com>
2026-01-10 10:40:17 +0530

    Finalize settings for Render PostgreSQL

--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = 'ikhedut_portal.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=not DEBUG
+        ssl_require=True
     )
 }
 
